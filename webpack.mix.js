c9dea534a0d2f03b98ab25dd5fdef9393d0f85a9
rakesh kumar <rakesh@froiden.com>
2018-06-12 15:20:10 +0530

    Change: directory restructured

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,10 +10,14 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.sass('resources/sass/app.scss', 'css')
-  .js('resources/js/app.js', 'js')
+var html = ['resources/index.html', 'resources/services.html', 'resources/services21.html' ];
+mix.sass('resources/sass/app.scss', 'resources/css')
+ //.js('resources/js/app.js', 'js')
   .options({processCssUrls: false})
-  .sourceMaps(true, 'source-map');
+  .sourceMaps(true, 'source-map')
   // .version();
   // .browserSync()
+  .copy('resources/images', 'dist/images')
+  .copy('resources/css', 'dist/css')
+  .copy('resources/js', 'dist/js')
+  .copy(html, 'dist');
